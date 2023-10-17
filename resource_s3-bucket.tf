bd03104c79369515faac04737234f6fb53a6305f
arrow <alparslanu6347@gmail.com>
2023-10-17 21:53:41 +0300

    updated

--- resource_s3-bucket.tf
+++ resource_s3-bucket.tf
@@ -1,58 +1,66 @@
+// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "kittens-bucket" {
-  bucket = var.bucket-name
+  bucket        = var.bucket-name
+  force_destroy = true // Default:false,  Normally it must be false. Because if we delete s3 mistakenly, we loose all of the states.
 }
 
-resource "aws_s3_object" "index" {
-  bucket = aws_s3_bucket.kittens-bucket.id
-  key    = "index.html"
-  source = "C:/Users/LEVENT/Desktop/Project-006-kittens-TERRAFOM/static-web/index.html"
-  acl    = "public-read"
-}
 
-resource "aws_s3_object" "photos" {
-  for_each = fileset("C:/Users/LEVENT/Desktop/Project-006-kittens-TERRAFOM/static-web/", "*")
-  bucket   = aws_s3_bucket.kittens-bucket.id
-  key      = each.value
-  source   = "C:/Users/LEVENT/Desktop/Project-006-kittens-TERRAFOM/static-web/${each.value}"
-  etag     = filemd5("C:/Users/LEVENT/Desktop/Project-006-kittens-TERRAFOM/static-web/${each.value}")
-  acl      = "public-read" ####
-}
-
-resource "aws_s3_bucket_website_configuration" "kittens-websconfig" {
+// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
+resource "aws_s3_bucket_website_configuration" "website-config" {
   bucket = aws_s3_bucket.kittens-bucket.id
 
   index_document {
     suffix = "index.html"
   }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "version_enable" {
+  bucket = aws_s3_bucket.kittens-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 
-# resource "aws_s3_bucket_policy" "kittens-policy" { ### AYDIN=> 2012-10-17  , "Principal" : {"AWS": "*"}
-#   bucket = aws_s3_bucket.kittens-bucket.id
-#   policy = <<POLICY
-# {
-#     "Version": "2012-10-17", 
-#     "Statement": [
-#         {
-#             "Sid"       : "AllowEveryoneReadOnlyAccess",
-#             "Effect"    : "Allow",
-#             "Principal" : "*" ,
-#             "Action"    : [ "s3:GetObject" ],
-#             "Resource"  : [ "arn:aws:s3:::${aws_s3_bucket.kittens-bucket.bucket}/*" ]
-#         }
-#     ]
-# }
-# POLICY
-# }
+resource "aws_s3_bucket_public_access_block" "blockkkk" {
+  bucket                  = aws_s3_bucket.kittens-bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
 
+// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+// https://developer.hashicorp.com/terraform/language/functions/fileset
+resource "aws_s3_object" "index" {     
+  bucket = aws_s3_bucket.kittens-bucket.id
+  key    = "index.html"
+  source = "${path.module}/static-web/index.html"
+  etag   = filemd5("${path.module}/static-web/index.html")
+}
 
-resource "aws_s3_bucket_policy" "bucket_policy" { ###
+resource "aws_s3_object" "photos" {
+  for_each = fileset("${path.module}/static-web/", "*.jpg") // * --> index.html + photos
+  bucket   = aws_s3_bucket.kittens-bucket.id
+  key      = each.value
+  source   = "${path.module}/static-web/${each.value}"
+  etag     = filemd5("${path.module}/static-web/${each.value}")
+}
+
+
+# S3 bucket policy-1
+resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.kittens-bucket.id
   policy = data.aws_iam_policy_document.allow_access.json
 }
 
 data "aws_iam_policy_document" "allow_access" {
+  version = "2012-10-17"
   statement {
     actions = ["s3:GetObject"]
     sid     = "PublicReadGetObject"
@@ -62,19 +70,40 @@ data "aws_iam_policy_document" "allow_access" {
       identifiers = ["*"]
     }
     resources = [
-      "arn:aws:s3:::${aws_s3_bucket.kittens-bucket.bucket}/*",
+      "${aws_s3_bucket.kittens-bucket.arn}/*" // "arn:aws:s3:::${aws_s3_bucket.kittens-bucket.bucket}/*"
+      //  "arn:aws:s3:::${aws_s3_bucket.kittens-bucket.id}/*"
     ]
   }
 }
 
-resource "aws_s3_bucket_acl" "bucket-acl" {
+data "aws_canonical_user_id" "current" {}
+
+
+resource "aws_s3_bucket_ownership_controls" "ownershipsss" {
   bucket = aws_s3_bucket.kittens-bucket.id
-  acl    = "public-read"
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.ownershipsss]
 
+  bucket = aws_s3_bucket.kittens-bucket.id
+  access_control_policy {
+    grant {
+      grantee {
+        id   = data.aws_canonical_user_id.current.id
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
 
-output "kittens_bucket_website-name" {
-  value = "http://${aws_s3_bucket.kittens-bucket.id}.s3-website-us-east-1.amazonaws.com"
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+  }
 }
+
+
