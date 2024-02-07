de373559526d394a8b0fe6800e47f597d5f09c5c
alparslanu6347 <alparslanu6347@gmail.com>
2024-02-07 21:16:17 +0300

    variable.tf updated

--- variable.tf
+++ variable.tf
@@ -1,12 +1,12 @@
 variable "domain" {
-  default     = "kittens.devopsarrow.com"   // write your domain
+  default     = "kittens.devopsarrow.com"   // write your domain (devopsarrow.com)
   type        = string
   description = "The domain name for the website."
 }
 
 
 variable "bucket-name" {
-  default     = "kittens.devopsarrow.com"   // write your bucket-name
+  default     = "kittens.devopsarrow.com"   // write your static website bucket-name (kittens.devopsarrow.com)
   type        = string
   description = "The name of the bucket without the www. prefix. Normally domain_name."
 }
